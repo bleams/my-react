74885fa4f202f5d357fa4c9fdfcca6c3da4e84d2
Georges Mbella <gmbellambe001@LU.EMA.AD.PWCINTERNAL.COM>
2016-12-29 11:48:22 +0100

    splitted structure

---
+++ fullstack-react/voting_app/app.js
@@ -0,0 +1,58 @@
+
+
+const ProductList = () => {
+    return (
+        <div className="ui items">
+            <Product />
+        </div>
+
+    );
+};
+
+// class ProductList extends React.Component {
+//   render() {
+//     return (
+//       <div className="ui items">
+//             <p>hehehehe</p>
+//           <Product />
+//       </div>
+
+//     );
+//   }
+// }
+
+
+
+const Product = () => {
+    return (
+        <div className='item'>
+            <div className='image'>
+                <img src='images/products/image-aqua.png' />
+            </div>
+            <div className='middle aligned content'>
+                <div className='description'>
+                    <a>Fort Knight</a>
+                    <p>Authentic renaissance actors, delivered in just two weeks.</p>
+                </div>
+                <div className='extra'>
+                    <span>Submitted by:</span>
+                    <img
+                        className='ui avatar image'
+                        src='images/avatars/daniel.jpg'
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+// let Hello = React.createClass({
+//     render: function() {
+//         return <div>Hello {this.props.name}</div>;
+//     }
+// });
+
+.render(
+    <ProductList />,
+    docment.getElementById('content')
+);
