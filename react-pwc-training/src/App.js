7ee8db8651cd01ba4fe19398ec8ca3b1394d669d
Georges Mbella Mbella <gmbellambe001@LU.EMA.AD.PWCINTERNAL.COM>
2016-12-29 18:10:46 +0100

    productlist rendered

--- react-pwc-training/src/App.js
+++ react-pwc-training/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import {data} from './data';
+import { data } from './data';
 
 class ProductList extends React.Component {
   render() {
-    const prod = data[0];
+    const products = data.map((prod) => {
+      return(
+        <Product
+        key={'product-' + prod.id}
+        id={prod.id}
+        title={prod.title}
+        description={prod.description}
+        url={prod.url}
+        votes={prod.votes}
+        submitter_avatar_url={prod.submitter_avatar_url}
+        product_image_url={prod.product_image_url}
+      />
+      )
+    });
+    console.log(products);
     return (
       <div className="ui items">
-          <Product 
-            id={prod.id} 
-            title={prod.title} 
-            description={prod.description} 
-            url={prod.url} 
-            votes={prod.votes} 
-            submitter_avatar_url={prod.submitter_avatar_url} 
-            product_image_url={prod.product_image_url}
-          
-          />
+        {products}
       </div>
 
     );
@@ -25,27 +30,31 @@ class ProductList extends React.Component {
 export default ProductList;
 
 class Product extends React.Component {
-    render(){
-      return (
+  render() {
+    return (
       <div className='item'>
         <div className='image'>
-          <img src={this.props.product_image_url} role="presentation"/>
+          <img src={this.props.product_image_url} role="presentation" />
         </div>
         <div className='middle aligned content'>
           <div className='header'>
             <a><i className='large caret up icon'></i></a>
-            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+            {this.props.votes}
           </div>
+
+          <div className='description'>
+            <a href="{this.props.url}">{this.props.title}</a>
+          </div>
+
           <div className='extra'>
             <span>Submitted by:</span>
             <img
               className='ui avatar image'
-              src='images/avatars/daniel.jpg'
-              role="presentation"
+              src={this.props.submitter_avatar_url}
               />
           </div>
         </div>
       </div>
     );
-    }
+  }
 };
